9834c7437dca9c8e5eef01497128b200c976bc1e
ikbalkaya <ceikbal@gmail.com>
2022-01-27 18:04:21 +0000

    Added simple configuration to AblyGame

--- ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/AblyGame.kt
+++ ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/AblyGame.kt
@@ -1,7 +1,47 @@
 package com.ablylabs.ablygamesdk
 
-interface AblyGame {
+import io.ably.lib.realtime.AblyRealtime
+import io.ably.lib.realtime.CompletionListener
+import io.ably.lib.types.AblyException
+import io.ably.lib.types.ErrorInfo
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+const val GLOBAL_CHANNEL_NAME = "global"
+class AblyGame(private val apiKey:String){
+    private val ably = AblyRealtime(apiKey)
+
     //enter game --global
-    fun enter(player: GamePlayer)
-    fun exit(player: GamePlayer)
+    suspend fun enter(player: GamePlayer):Result<Unit>{
+        return suspendCoroutine {continuation->
+            ably.channels[GLOBAL_CHANNEL_NAME].presence.run {
+                enterClient(player.id, object : CompletionListener {
+                    override fun onSuccess() {
+                        continuation.resume(Result.success(Unit))
+                    }
+
+                    override fun onError(reason: ErrorInfo?) {
+                        continuation.resume(Result.failure(AblyException.fromErrorInfo(reason)))
+                    }
+                })
+
+            }
+        }
+    }
+    suspend fun leave(player: GamePlayer):Result<Unit>{
+        return suspendCoroutine {continuation->
+            ably.channels[GLOBAL_CHANNEL_NAME].presence.run {
+                leaveClient(player.id, object : CompletionListener {
+                    override fun onSuccess() {
+                        continuation.resume(Result.success(Unit))
+                    }
+
+                    override fun onError(reason: ErrorInfo?) {
+                        continuation.resume(Result.failure(AblyException.fromErrorInfo(reason)))
+                    }
+                })
+
+            }
+        }
+    }
 }

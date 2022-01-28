a76f32ee7ac4bf6a64da678e0408de457866a92a
ikbalkaya <ceikbal@gmail.com>
2022-01-28 11:29:41 +0000

    Remove unused imports

--- app/src/main/java/com/ablylabs/multiplayergame/MultiplayerGameApp.kt
+++ app/src/main/java/com/ablylabs/multiplayergame/MultiplayerGameApp.kt
@@ -1,8 +1,6 @@
 package com.ablylabs.multiplayergame
 
 import android.app.Application
-import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
 import android.util.Log
 import com.ablylabs.ablygamesdk.AblyGame
 

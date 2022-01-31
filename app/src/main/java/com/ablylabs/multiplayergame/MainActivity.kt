ad8dffd914c2eb9389cdb0bd08a2af1f66c57cb8
ikbalkaya <ceikbal@gmail.com>
2022-01-31 15:10:22 +0000

    Added a toolbar and text to show number of players

--- app/src/main/java/com/ablylabs/multiplayergame/MainActivity.kt
+++ app/src/main/java/com/ablylabs/multiplayergame/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         recyclerLayoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         roomsRecyclerView = findViewById(R.id.roomsRecyclerView)

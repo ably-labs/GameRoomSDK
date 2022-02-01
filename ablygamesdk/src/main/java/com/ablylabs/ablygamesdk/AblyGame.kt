b2d41e441fae1d1f1fb682c11aba7041b53cf194
ikbalkaya <ceikbal@gmail.com>
2022-02-01 10:43:05 +0000

    Added number of players and observed changes

--- ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/AblyGame.kt
+++ ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/AblyGame.kt
@@ -6,8 +6,10 @@ import io.ably.lib.realtime.ConnectionState
 import io.ably.lib.realtime.ConnectionStateListener
 import io.ably.lib.types.AblyException
 import io.ably.lib.types.ErrorInfo
+import io.ably.lib.types.PresenceMessage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
@@ -16,10 +18,12 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.internal.ChannelFlow
 import kotlinx.coroutines.launch
+import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 const val GLOBAL_CHANNEL_NAME = "global"
+enum class PresenceAction{ENTER,LEAVE}
 
 class AblyGame private constructor(apiKey: String, gameOn: (ablyGame: AblyGame) -> Unit) {
     class Builder(val apiKey: String){
@@ -75,5 +79,23 @@ class AblyGame private constructor(apiKey: String, gameOn: (ablyGame: AblyGame) 
 
             }
         }
+    }
+
+    suspend fun numberOfPlayers():Int{
+        return suspendCoroutine { continuation->
+            continuation.resume(ably.channels[GLOBAL_CHANNEL_NAME].presence.get().size)
+        }
+    }
+
+
+    fun subscribeToPlayerNumberUpdate(updated: (action:PresenceAction) -> Unit) {
+        val observedActions = EnumSet.of(PresenceMessage.Action.enter, PresenceMessage.Action.leave)
+        ably.channels[GLOBAL_CHANNEL_NAME].presence.subscribe(observedActions) {
+            when (it.action) {
+                PresenceMessage.Action.enter -> updated(PresenceAction.ENTER)
+                PresenceMessage.Action.leave -> updated(PresenceAction.LEAVE)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/ablylabs/multiplayergame/MainActivity.kt
+++ app/src/main/java/com/ablylabs/multiplayergame/MainActivity.kt
@@ -2,13 +2,16 @@ package com.ablylabs.multiplayergame
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.delay
+import com.ablylabs.ablygamesdk.AblyGame
+import com.ablylabs.ablygamesdk.PresenceAction
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 private const val TAG = "MainActivity"
@@ -19,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerLayoutManager:LinearLayoutManager
     private lateinit var roomsRecyclerView: RecyclerView
+    private lateinit var ablyGame:AblyGame
+    private var numberOfPlayers = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,9 +40,28 @@ class MainActivity : AppCompatActivity() {
         roomsRecyclerView.addItemDecoration(dividerItemDecoration)
         roomsRecyclerView.adapter = RoomsRecyclerViewAdapter(sampleRooms)
 
+        val numberOfPlayersTextView = findViewById<TextView>(R.id.numberOfPlayersTextView)
+
         lifecycleScope.launch {
-            val result = MultiplayerGameApp.instance.ablyGameBuilder.build().enter(MyGamePlayer("ikbal"))
-            Log.d(TAG, "enter result $result ")
+            ablyGame = MultiplayerGameApp.instance.ablyGameBuilder.build()
+            //also register to changes
+            ablyGame.subscribeToPlayerNumberUpdate {
+                //you can either update numbers here or pull numberOfPlayers
+                when(it){
+                    PresenceAction.ENTER -> numberOfPlayers++
+                    PresenceAction.LEAVE -> numberOfPlayers--
+                }
+                numberOfPlayersTextView.text = "${numberOfPlayers} players"
+            }
+            val enterResult = ablyGame.enter(MyGamePlayer("ikbal${Random.nextInt(1000)}"))
+            if (enterResult.isSuccess){
+                Log.d(TAG, "Successful entry")
+            }else{
+                Log.e(TAG, "problem entering: ",enterResult.exceptionOrNull())
+            }
+            numberOfPlayers = ablyGame.numberOfPlayers()
+            numberOfPlayersTextView.text = "${numberOfPlayers} players"
+
         }
     }
 }

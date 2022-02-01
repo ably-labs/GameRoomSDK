e0e8f280d83993d5e3e8f24f2cf32ded573ac813
ikbalkaya <ceikbal@gmail.com>
2022-02-01 16:43:41 +0000

    Send message to player implementation

--- ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/GameRoomController.kt
+++ ablygamesdk/src/main/java/com/ablylabs/ablygamesdk/GameRoomController.kt
@@ -4,6 +4,7 @@ import io.ably.lib.realtime.AblyRealtime
 import io.ably.lib.realtime.CompletionListener
 import io.ably.lib.types.AblyException
 import io.ably.lib.types.ErrorInfo
+import io.ably.lib.types.Message
 import kotlinx.coroutines.flow.Flow
 import java.lang.Exception
 import kotlin.coroutines.resume
@@ -11,7 +12,7 @@ import kotlin.coroutines.suspendCoroutine
 
 sealed class RoomPresenceResult {
     data class Success(val gameRoom: GameRoom, val player: GamePlayer) : RoomPresenceResult()
-    data class Failure(val gameRoom: GameRoom, val player: GamePlayer, val exception:Exception?) : RoomPresenceResult()
+    data class Failure(val gameRoom: GameRoom, val player: GamePlayer, val exception: Exception?) : RoomPresenceResult()
 }
 
 //make result type names a bit more clear
@@ -20,7 +21,7 @@ typealias EnterRoomResult = RoomPresenceResult
 
 sealed class MessageSentResult {
     data class Success(val toWhom: GamePlayer) : MessageSentResult()
-    data class Failed(val toWhom: GamePlayer, val reason: String) : MessageSentResult()
+    data class Failed(val toWhom: GamePlayer, val exception: Exception?) : MessageSentResult()
 }
 
 data class ReceivedMessage(val from: GamePlayer, val message: String)
@@ -34,10 +35,12 @@ interface GameRoomController {
     suspend fun numberOfPeopleInRoom(gameRoom: GameRoom): Int
     suspend fun enter(player: GamePlayer, gameRoom: GameRoom): EnterRoomResult
     suspend fun leave(player: GamePlayer, gameRoom: GameRoom): LeaveRoomResult
+
+    //consider migrating message
     suspend fun sendMessageToPlayer(
         from: GamePlayer,
         to: GamePlayer,
-        message: GameMessage
+        message: String
     ): MessageSentResult
 
     suspend fun registerToTextMessage(room: GameRoom, receiver: GamePlayer): Flow<ReceivedMessage>
@@ -49,6 +52,14 @@ interface GameRoomController {
 internal const val roomNamespace = "room"
 internal const val playerNamespace = "player"
 internal fun roomChannel(gameRoom: GameRoom) = "room:${gameRoom.id}"
+internal fun bidirectinalPlayerChannel(player1: GamePlayer, player2: GamePlayer): String {
+    //this should return thee same regardless of playerIds, so it is a good idea to use them by their order
+    val comparisonResult = player1.id.compareTo(player2.id)
+    if (comparisonResult > 0) {
+        return "player:${player2.id}-${player1.id}"
+    }
+    return "player:${player1.id}-${player2.id}"
+}
 
 internal class GameRoomControllerImpl(private val ably: AblyRealtime) : GameRoomController {
     override suspend fun numberOfPeopleInRoom(gameRoom: GameRoom): Int {
@@ -62,11 +73,17 @@ internal class GameRoomControllerImpl(private val ably: AblyRealtime) : GameRoom
             ably.channels[roomChannel(gameRoom)].presence.apply {
                 enterClient(player.id, "no_data", object : CompletionListener {
                     override fun onSuccess() {
-                        continuation.resume(RoomPresenceResult.Success(gameRoom,player))
+                        continuation.resume(RoomPresenceResult.Success(gameRoom, player))
                     }
 
                     override fun onError(reason: ErrorInfo?) {
-                        continuation.resume(RoomPresenceResult.Failure(gameRoom,player,AblyException.fromErrorInfo(reason)))
+                        continuation.resume(
+                            RoomPresenceResult.Failure(
+                                gameRoom,
+                                player,
+                                AblyException.fromErrorInfo(reason)
+                            )
+                        )
                     }
                 })
 
@@ -75,15 +92,21 @@ internal class GameRoomControllerImpl(private val ably: AblyRealtime) : GameRoom
     }
 
     override suspend fun leave(player: GamePlayer, gameRoom: GameRoom): LeaveRoomResult {
-        return suspendCoroutine {continuation->
+        return suspendCoroutine { continuation ->
             ably.channels[roomChannel(gameRoom)].presence.apply {
                 leaveClient(player.id, "no_data", object : CompletionListener {
                     override fun onSuccess() {
-                        continuation.resume(RoomPresenceResult.Success(gameRoom,player))
+                        continuation.resume(RoomPresenceResult.Success(gameRoom, player))
                     }
 
                     override fun onError(reason: ErrorInfo?) {
-                        continuation.resume(RoomPresenceResult.Failure(gameRoom,player,AblyException.fromErrorInfo(reason)))
+                        continuation.resume(
+                            RoomPresenceResult.Failure(
+                                gameRoom,
+                                player,
+                                AblyException.fromErrorInfo(reason)
+                            )
+                        )
                     }
                 })
 
@@ -91,8 +114,25 @@ internal class GameRoomControllerImpl(private val ably: AblyRealtime) : GameRoom
         }
     }
 
-    override suspend fun sendMessageToPlayer(from: GamePlayer, to: GamePlayer, message: GameMessage): MessageSentResult {
-        TODO("Not yet implemented")
+    override suspend fun sendMessageToPlayer(from: GamePlayer, to: GamePlayer, messageText: String):
+            MessageSentResult {
+        //this message name should be derived from message when migrated
+        val message = Message("text", messageText, from.id)
+        //this should be a bidirectional channel between two players
+        val channelId = bidirectinalPlayerChannel(from, to)
+        return suspendCoroutine { continuation ->
+            ably.channels[channelId]
+                .publish(message, object : CompletionListener {
+                    override fun onSuccess() {
+                        continuation.resume(MessageSentResult.Success(to))
+                    }
+
+                    override fun onError(reason: ErrorInfo?) {
+                        continuation.resume(MessageSentResult.Failed(to, AblyException.fromErrorInfo(reason)))
+                    }
+                })
+        }
+
     }
 
     override suspend fun registerToTextMessage(room: GameRoom, receiver: GamePlayer): Flow<ReceivedMessage> {
